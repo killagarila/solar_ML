0dcaa6c2ae87b6f6e707bb99914a7a50ae98c760
y4-sayedtawfee <yasine2.sayedtawfeek@live.uwe.ac.uk>
2023-05-01 16:43:24 +0100

    Autism is my superpower

--- main2.py
+++ main2.py
@@ -18,7 +18,7 @@ from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDis
 from sklearn.svm import SVC, SVR
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.ensemble import AdaBoostClassifier
-import seaborn as sn
+import seaborn as sns
 
 from sklearn import metrics
 from sklearn.model_selection import GridSearchCV
@@ -30,6 +30,8 @@ from sklearn.metrics import accuracy_score, precision_score, classification_repo
 # %%
 readfile = pd.read_excel("solar-potential.xlsx", usecols=["Insolation", "Generation","Area"], nrows=10000)
 print(f"# num values:\n{readfile.isnull().sum()}\n")
+readfile = readfile[readfile.Generation <= 7000] 
+
 X = readfile[["Insolation", "Area"]].copy()
 y = readfile["Generation"].copy()
 print(X.head(), end="\n\n")
@@ -38,6 +40,23 @@ train_x, test_x, train_y, test_y = train_test_split(X,y, test_size=0.19)
 min_max_scalar = preprocessing.MinMaxScaler()
 X = min_max_scalar.fit_transform(X)
 # y = min_max_scalar.fit_transform(y)
+
+# %%
+# readfile.hist()
+# plt.show()
+
+# # %%
+# sns.boxplot(readfile["Generation"])
+
+# # %%
+# print(np.where(readfile["Generation"]>1000000))
+
+# # %%
+# fig, ax = plt.subplots(figsize = (18,10))
+
+# ax.scatter(readfile["Generation"], readfile["Area"])
+
+# plt.show()
 
 # # %%
 # rfc = RandomForestClassifier(n_estimators=50, max_features="sqrt", max_samples=None)
@@ -51,15 +70,15 @@ X = min_max_scalar.fit_transform(X)
 # cm = confusion_matrix(test_y, pred2, labels = rfc.classes_)
 # disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=rfc.classes_)
 # disp.plot()
-# # fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 8))
-# # rfc_disp = RocCurveDisplay.from_estimator(rfc, test_x, test_y, ax=ax1)
-# # prec, recall, _ = precision_recall_curve(test_y, pred2, pos_label=rfc.classes_[1])
-# # pr_display = PrecisionRecallDisplay(precision=prec, recall=recall)
+# fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 8))
+# rfc_disp = RocCurveDisplay.from_estimator(rfc, test_x, test_y, ax=ax1)
+# prec, recall, _ = precision_recall_curve(test_y, pred2, pos_label=rfc.classes_[1])
+# pr_display = PrecisionRecallDisplay(precision=prec, recall=recall)
 
 # print(classification_report(test_y, pred2, target_names=["suitable", "well suitable", "excellent suitable"]))
 
 
-# # %%
+# %%
 # regr = make_pipeline(StandardScaler(), SVR(C=1.0, degree=3, epsilon=20))
 # regr.fit(train_x, train_y)
 # pred3 = regr.predict(test_x)
@@ -96,8 +115,8 @@ from mpl_toolkits.mplot3d import Axes3D
 
 fig = plt.figure(figsize=(20,10))
 ### Set figure size
-ax = fig.gca(111, projection='3d')
-ax.scatter(readfile['Insolation'],readfile['Area'],readfile['Generation'],c='red', marker='o', alpha=0.5)
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(test_x['Insolation'],test_x['Area'],test_y,c='red', marker='o', alpha=0.5)
 ax.plot_surface(x_surf,y_surf,fittedY.reshape(x_surf.shape), color='b', alpha=0.3)
 ax.set_xlabel('Insolation')
 ax.set_ylabel('Area')
@@ -105,5 +124,3 @@ ax.set_zlabel('Generation')
 plt.show()
 
 
-
-# %%
